d12bfeed43f29bbd3665001eb93c3a311e8314d0
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-11-28 18:32:42 -0500

    Background CSS themes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-violet-50 to-violet-400 dark:bg-gradient-to-t dark:from-gray-900 dark:to-violet-950">
+      <Header />
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center">
-        <a className="p-6 mt-6 text-center border w-96">
+        <a className="p-6 mt-6 text-center w-96">
           <Image
             src="/leaderboards.png"
             alt="Next.js"
