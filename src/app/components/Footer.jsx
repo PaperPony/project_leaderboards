8742b5c85ac2d3e41b3a5f99ecccb8bd0a26621e
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-11-29 17:58:30 -0500

    Footer Implementation

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="items-center p-4 bottom-0 bg-violet-400 dark:bg-gray-900">
+      <h1 className="text-center text-slate-800 dark:text-slate-200 font-bold text-xl">
+        Made by Areeb and The Areebers
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-violet-50 to-violet-400 dark:bg-gradient-to-t dark:from-gray-900 dark:to-violet-950"></main>
+      <Footer />
     </div>
   );
 }
