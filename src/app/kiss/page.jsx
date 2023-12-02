d12a0008fcd73c37260809e89ecdae5a85972abe
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-12-02 13:10:03 -0500

    Kiss page

---
+++ src/app/kiss/page.jsx
@@ -0,0 +1,12 @@
+// import the fakisswinkheart icon
+import { FaKissWinkHeart } from "react-icons/fa";
+
+const Page = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-violet-200 to-violet-400 dark:bg-gradient-to-t dark:from-gray-900 dark:to-violet-950">
+      <FaKissWinkHeart className="text-9xl text-center text-gray-800 dark:text-white" />
+    </div>
+  );
+};
+
+export default Page;
