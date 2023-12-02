51a56cdef89b6e8f0bb0471f1c295c3100f48f12
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-12-02 13:09:42 -0500

    Not Found Page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-violet-200 to-violet-400 dark:bg-gradient-to-t dark:from-gray-900 dark:to-violet-950">
+      <p className="text-4xl font-bold text-center text-gray-800 dark:text-white">
+        Whoops! The page you are looking for doesn`t exist!
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
