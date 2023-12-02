a95693cdd30e208444c9a13522cbf0c02c4bdb2d
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-12-02 12:07:07 -0500

    Update layout.jsx

--- src/app/games/layout.jsx
+++ src/app/games/layout.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const layout = ({ children }) => {
   return (
-    <div>
-      <div className="text-3xl font-bold text-center">Game layout.</div>
+    <div className="h-full text-3xl font-bold text-center p-24 bg-gradient-to-b from-violet-50 to-violet-400 dark:bg-gradient-to-t dark:from-gray-900 dark:to-violet-950">
       <div>{children}</div>
     </div>
   );
