3e55edfd35df771787a7b766c50750d9d5f026a5
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-12-03 14:10:29 -0500

    User greeting for home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,20 @@
 export default function Home() {
   return (
     <div>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-violet-200 to-violet-400 dark:bg-gradient-to-t dark:from-gray-900 dark:to-violet-950">
-        <div>Home Page</div>
+      <main className="flex min-h-screen flex-col items-center p-24 bg-gradient-to-b from-violet-200 to-violet-400 dark:bg-gradient-to-t dark:from-gray-900 dark:to-violet-950">
+        <div className="flex flex-col items-center space-y-10">
+          <h1 className="text-4xl font-bold text-center text-black dark:text-white">
+            Welcome to our website!
+          </h1>
+          <p className="text-xl text-center text-black dark:text-white">
+            We are a group of students who are learning good design for user
+            interfaces!
+          </p>
+          <p className="text-xl text-center text-black dark:text-white">
+            We are currently working on a project for our class, and we hope you
+            enjoy it!
+          </p>
+        </div>
       </main>
     </div>
   );
