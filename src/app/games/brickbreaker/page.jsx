4a845763af9bc6aad2681a7a2cd4a230e881e647
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-12-03 14:13:51 -0500

    Update brick breaker to comment

--- src/app/games/brickbreaker/page.jsx
+++ src/app/games/brickbreaker/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const BrickBreaker = () => {
-  return <div>BrickBreaker Game.</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      To Be Implemented!
+    </div>
+  );
 };
 
 export default BrickBreaker;
