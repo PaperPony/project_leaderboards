e0f3d72526cd3962d720ae46e3edd730aab71b43
Kashmeeber <areeb.ch2001@gmail.com>
2023-12-04 18:07:04 -0500

    Finished snake game

--- src/app/games/snake/page.jsx
+++ src/app/games/snake/page.jsx
@@ -1,221 +1,225 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const Snake = () => {
-  // const [snake, setSnake] = useState([{ x: 1, y: 1 }]);
-  // const [food, setFood] = useState({ x: 1, y: 1 });
-  // const [direction, setDirection] = useState('RIGHT');
-  // const [score, setScore] = useState(0);
-  // const [gameOver, setGameOver] = useState(false);
-  // const [obstacles, setObstacles] = useState([]);
-  // const [gridSize, setGridSize] = useState(15);
+const GRID_SIZE = 20;
+const TICK_INTERVAL = 150; // in milliseconds
 
-  // useEffect(() => {
-  //   const initialObstacles = Array.from({ length: 3 }).map(() => {
-  //     const x = Math.floor(Math.random() * (gridSize - 2));
-  //     const y = Math.floor(Math.random() * (gridSize - 2));
-  //     const isVertical = Math.random() < 0.5;
+const SnakeGame = () => {
+  const generateRandomFood = () => {
+    return {
+      x: Math.floor(Math.random() * GRID_SIZE),
+      y: Math.floor(Math.random() * GRID_SIZE),
+    };
+  };
 
-  //     return isVertical
-  //       ? [{ x, y }, { x, y: y + 1 }, { x, y: y + 2 }]
-  //       : [{ x, y }, { x: x + 1, y }, { x: x + 2, y }];
-  //   }).flat();
+  const generateRandomObstacle = () => {
+    return {
+      x: Math.floor(Math.random() * GRID_SIZE),
+      y: Math.floor(Math.random() * GRID_SIZE),
+    };
+  };
 
-  //   const borderObstacles = Array.from({ length: gridSize }).flatMap((_, i) => [
-  //     { x: i, y: 0 },
-  //     { x: i, y: gridSize - 1 },
-  //     { x: 0, y: i },
-  //     { x: gridSize - 1, y: i },
-  //   ]);
+  const [snake, setSnake] = useState([{ x: 0, y: 0 }]);
+  const [direction, setDirection] = useState("RIGHT");
+  const [food, setFood] = useState(generateRandomFood());
+  const [obstacles, setObstacles] = useState(
+    Array.from({ length: 5 }, generateRandomObstacle)
+  );
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
 
-  //   setObstacles([...initialObstacles, ...borderObstacles]);
-  //   setFood(generateFoodCoordinates());
-  // }, [gridSize]);
+  const canvasRef = useRef(null);
 
-  // useEffect(() => {
-  //   if (gameOver) return;
+  const moveSnake = () => {
+    const head = { ...snake[0] };
 
-  //   const interval = setInterval(() => {
-  //     moveSnake();
-  //   }, 200);
+    switch (direction) {
+      case "UP":
+        head.y -= 1;
+        break;
+      case "DOWN":
+        head.y += 1;
+        break;
+      case "LEFT":
+        head.x -= 1;
+        break;
+      case "RIGHT":
+        head.x += 1;
+        break;
+      default:
+        break;
+    }
 
-  //   window.addEventListener('keydown', updateDirection);
+    // Check if the new head position is outside the boundaries
+    if (
+      head.x < 0 ||
+      head.x >= GRID_SIZE ||
+      head.y < 0 ||
+      head.y >= GRID_SIZE
+    ) {
+      setGameOver(true);
+      return;
+    }
 
-  //   return () => {
-  //     clearInterval(interval);
-  //     window.removeEventListener('keydown', updateDirection);
-  //   };
-  // }, [snake, direction, food, score, gameOver]);
+    // Check if the new head position is on an obstacle
+    if (
+      obstacles.some(
+        (obstacle) => obstacle.x === head.x && obstacle.y === head.y
+      )
+    ) {
+      setGameOver(true);
+      return;
+    }
 
-  // useEffect(() => {
-  //   setGridSize(Math.max(15, Math.ceil(snake.length / 2) * 2 + 1));
-  // }, [snake]);
+    // Check if the new head position collides with the snake's body
+    if (
+      snake
+        .slice(1)
+        .some((segment) => segment.x === head.x && segment.y === head.y)
+    ) {
+      setGameOver(true);
+      return;
+    }
 
-  // const updateDirection = (event) => {
-  //   switch (event.key) {
-  //     case 'ArrowUp':
-  //     case 'w':
-  //     case 'W':
-  //       setDirection('UP');
-  //       break;
-  //     case 'ArrowDown':
-  //     case 's':
-  //     case 'S':
-  //       setDirection('DOWN');
-  //       break;
-  //     case 'ArrowLeft':
-  //     case 'a':
-  //     case 'A':
-  //       setDirection('LEFT');
-  //       break;
-  //     case 'ArrowRight':
-  //     case 'd':
-  //     case 'D':
-  //       setDirection('RIGHT');
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
+    // Check if the new head position is on the food
+    const ateFood = head.x === food.x && head.y === food.y;
 
-  // const moveSnake = () => {
-  //   if (gameOver) return;
+    const newSnake = ateFood
+      ? [{ x: food.x, y: food.y }, ...snake]
+      : [head, ...snake.slice(0, -1)];
+    setSnake(newSnake);
 
-  //   const newSnake = snake.map((s) => ({ ...s }));
-  //   const head = { ...newSnake[0] };
+    if (ateFood) {
+      setScore((prevScore) => prevScore + 1);
+      setFood(generateRandomFood());
+    }
+  };
 
-  //   switch (direction) {
-  //     case 'UP':
-  //       head.y -= 1;
-  //       break;
-  //     case 'DOWN':
-  //       head.y += 1;
-  //       break;
-  //     case 'LEFT':
-  //       head.x -= 1;
-  //       break;
-  //     case 'RIGHT':
-  //       head.x += 1;
-  //       break;
-  //     default:
-  //       break;
-  //   }
+  const checkFood = () => {
+    const head = snake[0];
 
-  //   if (
-  //     head.x < 0 ||
-  //     head.y < 0 ||
-  //     head.x >= gridSize ||
-  //     head.y >= gridSize ||
-  //     obstacles.some((obstacle) => obstacle.x === head.x && obstacle.y === head.y) ||
-  //     newSnake.some((segment) => segment.x === head.x && segment.y === head.y)
-  //   ) {
-  //     endGame();
-  //     return;
-  //   }
+    // Check if snake eats food
+    if (head.x === food.x && head.y === food.y) {
+      const newSnake = [{ x: food.x, y: food.y }, ...snake];
+      setFood(generateRandomFood());
+      setSnake(newSnake);
+    }
+  };
 
-  //   newSnake.unshift(head);
+  useEffect(() => {
+    if (gameOver) return;
 
-  //   if (head.x === food.x && head.y === food.y) {
-  //     setFood(generateFoodCoordinates());
-  //     setScore((prevScore) => prevScore + 1);
-  //   } else {
-  //     newSnake.pop();
-  //   }
+    const handleKeyPress = (e) => {
+      switch (e.key.toLowerCase()) {
+        case "arrowup":
+        case "w":
+          setDirection("UP");
+          break;
+        case "arrowdown":
+        case "s":
+          setDirection("DOWN");
+          break;
+        case "arrowleft":
+        case "a":
+          setDirection("LEFT");
+          break;
+        case "arrowright":
+        case "d":
+          setDirection("RIGHT");
+          break;
+        default:
+          break;
+      }
+    };
 
-  //   setSnake(newSnake);
-  // };
+    const handleTick = () => {
+      moveSnake();
+      checkFood();
+    };
 
-  // const endGame = () => {
-  //   setGameOver(true);
-  // };
+    document.addEventListener("keydown", handleKeyPress);
 
-  // const restartGame = () => {
-  //   setSnake([{ x: 1, y: 1 }]);
-  //   setFood(generateFoodCoordinates());
-  //   setDirection('RIGHT');
-  //   setScore(0);
-  //   setGameOver(false);
-  // };
+    const tickInterval = setInterval(handleTick, TICK_INTERVAL);
 
-  // function generateFoodCoordinates() {
-  //   let newFood;
-  //   do {
-  //     newFood = {
-  //       x: Math.floor(Math.random() * gridSize),
-  //       y: Math.floor(Math.random() * gridSize),
-  //     };
-  //   } while (
-  //     obstacles.some((obstacle) => obstacle.x === newFood.x && obstacle.y === newFood.y) ||
-  //     snake.some((segment) => segment.x === newFood.x && segment.y === newFood.y)
-  //   );
-  //   return newFood;
-  // }
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+      clearInterval(tickInterval);
+    };
+  }, [snake, direction, food, gameOver, obstacles]);
 
-  // return (
-  //   <div className="text-4xl font-bold mb-4">
-  //     Snake
-  //     <div className="text-xl font-bold mb-2">Score: {score}</div>
-  //     <div className="relative flex flex-col items-center justify-center bg-gray-800 text-white">
-  //       {gameOver && (
-  //         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center game-over">
-  //           <h1 className="text-4xl font-bold mb-4">Game Over!</h1>
-  //           <p className="text-lg mb-8">Your score: {score}</p>
-  //           <button
-  //             className="bg-blue-500 text-white px-4 py-2 rounded-md restart-button"
-  //             onClick={restartGame}
-  //           >
-  //             Restart Game
-  //           </button>
-  //         </div>
-  //       )}
-  //       <div
-  //         className={`grid grid-cols-${gridSize} gap-1`}
-  //         style={{
-  //           maxWidth: `${gridSize * 5 + (gridSize - 1)}rem`, // Adjusted width based on grid size
-  //         }}
-  //       >
-  //         {Array.from({ length: gridSize * gridSize }).map((_, index) => (
-  //           <div
-  //             key={index}
-  //             className="w-5 h-5 bg-gray-700 border border-gray-800"
-  //           />
-  //         ))}
-  //         {obstacles.map((obstacle, index) => (
-  //           <div
-  //             key={index}
-  //             className="w-5 h-5 bg-red-800 border border-gray-800 obstacle"
-  //             style={{
-  //               gridColumn: obstacle.x + 1,
-  //               gridRow: obstacle.y + 1,
-  //             }}
-  //           />
-  //         ))}
-  //         {snake.map((segment, index) => (
-  //           <div
-  //             key={index}
-  //             className="w-5 h-5 bg-green-500 border border-gray-800 snake-segment"
-  //             style={{
-  //               gridColumn: segment.x + 1,
-  //               gridRow: segment.y + 1,
-  //             }}
-  //           />
-  //         ))}
-  //         <div
-  //           className="w-5 h-5 bg-red-500 border border-gray-800 food"
-  //           style={{
-  //             gridColumn: food.x + 1,
-  //             gridRow: food.y + 1,
-  //           }}
-  //         />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawSnake(ctx);
+    drawFood(ctx);
+    drawObstacles(ctx);
+  }, [snake, food, obstacles, gameOver]);
+
+  const drawSnake = (ctx) => {
+    const head = snake[0];
+
+    // Draw head
+    ctx.fillStyle = "darkgreen";
+    ctx.fillRect(head.x * 20, head.y * 20, 20, 20);
+
+    // Draw body
+    ctx.fillStyle = "green";
+    snake.slice(1).forEach((segment) => {
+      ctx.fillRect(segment.x * 20, segment.y * 20, 20, 20);
+    });
+  };
+
+  const drawFood = (ctx) => {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(food.x * 20, food.y * 20, 20, 20);
+  };
+
+  const drawObstacles = (ctx) => {
+    ctx.fillStyle = "gray";
+    obstacles.forEach((obstacle) => {
+      ctx.fillRect(obstacle.x * 20, obstacle.y * 20, 20, 20);
+    });
+  };
+
+  const resetGame = () => {
+    setSnake([{ x: 0, y: 0 }]);
+    setDirection("RIGHT");
+    setFood(generateRandomFood());
+    setObstacles(Array.from({ length: 5 }, generateRandomObstacle));
+    setGameOver(false);
+    setScore(0);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center">
-      To Be Implemented!
+    <div className="flex flex-col items-center justify-center h-screen relative overflow-hidden">
+      <h1 className="text-4xl text-center text-white">Tic Tac Toe</h1>
+      <div className="flex flex-col items-center">
+        <p className="text-xl font-bold mb-2">Score: {score}</p>
+        <canvas
+          ref={canvasRef}
+          width={GRID_SIZE * 20}
+          height={GRID_SIZE * 20}
+          className="border-2 border-black bg-black"
+        />
+      </div>
+      {gameOver && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="bg-white p-4 rounded shadow-lg text-center">
+            <p className="text-3xl font-bold mb-2 text-black">Game Over!</p>
+            <button
+              className="bg-blue-500 text-white py-2 px-4 rounded"
+              onClick={resetGame}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Snake;
+export default SnakeGame;
