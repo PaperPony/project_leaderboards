5345e6a0462fcad638298864559defc74c05fae4
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-12-06 10:33:53 -0500

    Updating Home Link in Header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,18 +6,22 @@ const Header = () => {
   return (
     <nav className="overflow-hidden w-screen top-0">
       {/* Header */}
-      {/* Add a border to the bottom of the header */}
       <div className="flex flex-row justify-between items-center p-4 bg-purple-300 dark:bg-violet-950 bg-opacity-90 dark:bg-opacity-90 border-b-2 border-violet-200 dark:border-violet-900">
-        <Link href="/">
-          {/* Add the leaderboards png */}
-          <Image
-            src="/leaderboards.png"
-            width={100}
-            height={50}
-            alt="leaderboards-logo"
-          />
-        </Link>
-        {/* Add the centered text, "Welcome To Project Leaderboards" */}
+        <div className="flex items-center">
+          <Link href="/">
+            <Image
+              src="/leaderboards.png"
+              width={100}
+              height={50}
+              alt="leaderboards-logo"
+            />
+          </Link>
+          <Link href="/">
+            <div className="text-slate-800 dark:text-slate-200 font-bold text-2xl hidden md:block ml-4">
+              Home
+            </div>
+          </Link>
+        </div>
         <h1 className="text-center text-slate-800 dark:text-slate-200 font-bold text-2xl">
           Welcome To Project Leaderboards!
         </h1>
